e17ecc1aca50e149e3cc176b7af82d90a5ac38e7
daulet2030 <daulet2030@gmail.com>
2020-09-22 20:35:01 +0600

    simple query

--- src/test/java/LearningJDBC.java
+++ src/test/java/LearningJDBC.java
@@ -1,18 +1,25 @@
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class LearningJDBC {
     Connection connection;
 
-    @Test
+    @BeforeClass
     public void init() throws SQLException {
         String url = "jdbc:mysql://test.medis.mersys.io:3306/ts_dake";
         String username = "technostudy";
         String password = "zhTPis0l9#$&";
         connection = DriverManager.getConnection(url, username, password);
+    }
+
+    @Test
+    public void simpleSelectTest() throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select task_id from ts_tasks;");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString(1));
+        }
     }
 }
