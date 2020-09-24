c490496bc0f5c5c3de426a7017c115ffcd41f2b0
daulet2030 <daulet2030@gmail.com>
2020-09-24 21:10:36 +0600

    Task2 solutions

--- src/test/java/CompanyTest.java
+++ src/test/java/CompanyTest.java
@@ -1,3 +1,4 @@
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -26,6 +27,28 @@ public class CompanyTest {
             System.out.print(resultSet.getString("REGION_ID") + "\t");
             System.out.println();
         }
+    }
+    // Task2: create a test that tests that Egypt is in region 4+
+    // solution 1
+    @Test
+    public void egyptInRegion4() throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from countries;"); // selecting all the countries, not optimized
+        while (resultSet.next()) {
+            // if there's no Egypt this test will never fail, but it should
+            if(resultSet.getString("COUNTRY_NAME") =="Egypt"){
+                Assert.assertEquals(resultSet.getString("REGION_ID"),4);
+            }
+        }
+    }
+    // solution 2
+    @Test
+    public void simpleSelectGettingNullValuesAsWrappers() throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery( "SELECT * FROM company.countries where COUNTRY_NAME = 'Egypt';" );
+        resultSet.absolute( 1 );
+        int region_id = resultSet.getInt( "REGION_ID" );
+        Assert.assertEquals( region_id,4 );
     }
 
     @AfterClass
